702b803811edf44752924ac8d1cfd1e09831f2f9
webB1an <webB1anyaoyao@gmail.com>
2023-04-17 11:33:34 +0800

    fix: add assets

--- types/components.d.ts
+++ types/components.d.ts
@@ -9,7 +9,6 @@ export {}
 
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
-    HelloWorld: typeof import('./../src/components/HelloWorld.vue')['default']
     Loading: typeof import('./../src/components/Loading/index.vue')['default']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
